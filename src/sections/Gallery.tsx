227234627005c3d4a6e9c7351909db70ae7d5aee
arnausd23 <arnausd23@hotmail.com>
2024-02-10 20:28:34 +0100

    Added gallery on local image

--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -1,34 +1,22 @@
-import googleDriveIcon from "../assets/google-drive.png";
 import Button from "../components/Button";
+import googleDriveIcon from "../assets/google-drive.png";
+import gallery1 from "../assets/gallery1.webp";
+import gallery2 from "../assets/gallery2.webp";
+import gallery3 from "../assets/gallery3.webp";
+import gallery4 from "../assets/gallery4.webp";
+import gallery5 from "../assets/gallery5.webp";
+import gallery6 from "../assets/gallery6.webp";
 
 function Gallery() {
   return (
     <section id="gallery" className="relative">
       <section className="relative grid grid-cols-2 lg:grid-cols-3">
-        <img
-          src="https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=3538&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1595407753234-0882f1e77954?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHdlZGRpbmd8ZW58MHwwfDB8fHwy"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1623788452350-4c8596ff40bb?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1519691548119-14735e4a11c8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjB8fHdlZGRpbmclMjBkaW5uZXJ8ZW58MHwwfDB8fHwy"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1625038032515-308ab14d10b9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
+        <img src={gallery1} alt="Gallery example" />
+        <img src={gallery2} alt="Gallery example" />
+        <img src={gallery3} alt="Gallery example" />
+        <img src={gallery4} alt="Gallery example" />
+        <img src={gallery5} alt="Gallery example" />
+        <img src={gallery6} alt="Gallery example" />
         <div className="absolute top-0 bottom-0 left-0 right-0 bg-black opacity-65" />
         <div className="absolute top-1/2 -translate-y-1/2 lg:left-1/2 lg:center-absolute text-center p-12">
           <h2 className="text-xl lg:text-3xl text-white">
